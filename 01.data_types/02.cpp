0504765abf187b4a671a1ba582a11a0d9262c008
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-26 19:15:15 -0700

    add file: 02.cpp

---
+++ 01.data_types/02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//  float t, f;
+    float t;
+    cin >> t;
+//  f = (t * 9) / 5 + 32;
+    cout << (t * 9) / 5 + 32 << " Fahrenheit";
+    return 0;
+}
