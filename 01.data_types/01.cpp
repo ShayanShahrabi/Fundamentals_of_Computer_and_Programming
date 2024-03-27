83aaa069a2ccab194e48295bbea7e6c370735fe4
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-26 19:04:05 -0700

    add file: 01.cpp

---
+++ 01.data_types/01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "welcome to \"C++\" \nthis language is my love \nthe \'c++\' website    \\www.cplusplus.com\\ \nis a good source to learn!" << endl;
+    return 0;
+}
