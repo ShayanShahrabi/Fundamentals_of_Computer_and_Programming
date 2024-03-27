992978f76880eee970b352e20fe3cc7e7fb97ebc
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-26 20:05:57 -0700

    update file: 03.cpp

--- 01.data_types/03.cpp
+++ 01.data_types/03.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
     int shah, vazir, rokh, fil, asb, sarbaz;
     cin >> shah >> vazir >> rokh >> fil >> asb >> sarbaz;
-    cout << 1 - shah << " " << 1 - vazir << ' ' << 2 - rokh << ' ' << 2 - fil << ' ' << 2 - asb << ' ' << 8 - sarbaz << endl;
+    cout << 1 - shah << ' ' << 1 - vazir << ' ' << 2 - rokh << ' ' << 2 - fil << ' ' << 2 - asb << ' ' << 8 - sarbaz << endl;
     return 0;
 }
