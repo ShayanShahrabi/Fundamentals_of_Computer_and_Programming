761f7767a93c16b08aefa756f78d75026be09ed1
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-29 11:08:37 -0700

    update file: 02.cpp

--- 01.data_types/02.cpp
+++ 01.data_types/02.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main(){
-//  float t, f;
+    // int a = 99;
+    // float b = 99;
+    // cout << a / 10 << endl;
+    // cout << b / 10;
+
+    // float t, f;
     float t;
     cin >> t;
-//  f = (t * 9) / 5 + 32;
+    // f = (t * 9) / 5 + 32;
     cout << (t * 9) / 5 + 32 << " Fahrenheit";
     return 0;
 }
