8633a3f7d9dd5c75a490fc073762fae548c77e46
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-11-07 01:50:43 -0800

    update 12.cpp in 02.conditions

--- 02.conditions/cpp/12.cpp
+++ 02.conditions/cpp/12.cpp
@@ -2,17 +2,19 @@
 
 using namespace std;
 
-int main(){
+int main() {
     string day;
     cin >> day;
 
-    if (day == "monday" || day == "tuesday" ||
-        day == "thursday" || day == "friday" || day == "wednesday"){
-            cout << "weekday";
-    } else if (day == "saturday" || day == "sunday"){
-        cout << "weekend";
-    }else{
-        cout << "invalid day";
+    // Check if the day is a weekday or weekend
+    if (day == "monday" || day == "tuesday" || 
+        day == "wednesday" || day == "thursday" || 
+        day == "friday") {
+        cout << "weekday" << endl;
+    } else if (day == "saturday" || day == "sunday") {
+        cout << "weekend" << endl;
+    } else {
+        cout << "invalid day" << endl;
     }
 
     return 0;
